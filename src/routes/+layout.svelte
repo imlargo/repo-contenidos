5eb60fbd35e604e833d917be1be5f98f9c31f2c1
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-09-17 15:29:19 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '$styles/app.scss';
+
+	import type { Snippet } from 'svelte';
+	type Props = {
+		children: Snippet;
+	};
+	const { children }: Props = $props();
+</script>
+
+{@render children()}
