d61714cdb5aceb5d9dd48abb1ecc2e055378fa48
imlargo <jlargob@unal.edu.co>
2024-09-18 09:12:54 -0500

    feat: crear estructura base buscador

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+	import { Input } from '$lib/components/ui/input/index.js';
+
+
+    let codigoBusqueda = $state('');
+    async function search() {
+
+        if (codigoBusqueda === "") {
+            return;
+        }
+
+        await goto(`/asignatura?codigo=${codigoBusqueda}`);
+    }
+</script>
+
+<main class="py-24">
+	<section class="flex justify-center items-center">
+		<div class="flex gap-1">
+			<Input type="number" class="w-full" placeholder="Codigo asignatura" bind:value={codigoBusqueda} />
+			<button
+				class="inline-flex items-center gap-2 bg-black text-white font-semibold px-5 py-0.5 rounded-md cursor-pointer"
+                onclick={search}
+			>
+				<i class="bi bi-search leading-none"></i>
+				<span>Buscar</span>
+			</button>
+		</div>
+	</section>
+</main>
