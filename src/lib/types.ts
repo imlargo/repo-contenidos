1ef4e4ffb9c698c9eea1d38f7049778cd8ce30df
imlargo <jlargob@unal.edu.co>
2024-09-17 19:57:43 -0500

    chore: estructura base pagina de visualizacion

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,16 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export interface Asignatura {
+	codigo: string;
+	nombre: string;
+	uab: string;
+	vigente: boolean;
+	horasPresenciales: number;
+	horasNoPresenciales: number;
+	creditos: number;
+	validable: boolean;
+	electiva: boolean;
+	descripcion: string;
+	contenido: string;
+	planes_relacionados: string[];
+}
---
+++ src/routes/asignatura/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    type Props = {
+        data: PageData;
+    }
+    const { data }: Props = $props();
+
+    const asignatura = data.asignatura;
+</script>
+
+<div>
+    {asignatura.codigo}
+</div>
