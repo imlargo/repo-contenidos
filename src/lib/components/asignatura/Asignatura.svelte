5dc887f7ee9f0b5247e719938aa5b9ffd5b42783
imlargo <jlargob@unal.edu.co>
2025-01-23 09:02:00 -0500

    feat: update button click handlers to call updateAsignatura with descripcion and contenido

--- src/lib/components/asignatura/Asignatura.svelte
+++ src/lib/components/asignatura/Asignatura.svelte
@@ -226,7 +226,9 @@
 			<span>Descripción</span>
 		</h3>
 
-		<button class="btn-save">Guardar</button>
+		<button onclick={() => {
+			storeAsignatura.updateAsignatura(asignatura.id, { descripcion: asignatura.descripcion });
+		}} class="btn-save">Guardar</button>
 	</div>
 
 	{#if editMode}
@@ -257,7 +259,9 @@
 			<span>Contenido</span>
 		</h3>
 
-		<button class="btn-save">Guardar</button>
+		<button onclick={() => {
+			storeAsignatura.updateAsignatura(asignatura.id, { contenido: asignatura.contenido });
+		}} class="btn-save">Guardar</button>
 	</div>
 
 	{#if editMode}
