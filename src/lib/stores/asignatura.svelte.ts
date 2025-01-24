f01b7206602b5008de5ee2c75ad341a9509094bd
imlargo <jlargob@unal.edu.co>
2025-01-24 01:21:05 -0500

    feat: add updateFromAtenea method to StoreAsignatura for dynamic updates from Atenea service

--- src/lib/stores/asignatura.svelte.ts
+++ src/lib/stores/asignatura.svelte.ts
@@ -3,6 +3,7 @@ import type { Asignatura } from '$src/lib/types/asignatura';
 import { boolToAfirmacion, afirmacionToBool } from '$src/lib/utils/utils';
 import { dbController } from '$db/db';
 import { toast } from "svelte-sonner";
+import { AteneaService } from '../services/atenea';
 
 const detailsFields: (keyof Asignatura)[] = [
     "vigente",
@@ -25,7 +26,6 @@ export class StoreAsignatura {
     hasChangedDetails() {
         return detailsFields.some(field => this.asignatura[field] !== this.initialAsignatura[field]);
     }
-
 
     restoreInitialAsignaturaDetails() {
         for (const field of detailsFields) {
@@ -66,5 +66,40 @@ export class StoreAsignatura {
         for (const field of Object.keys(data)) {
             (this.initialAsignatura[field as keyof Asignatura] as any) = data[field as keyof Asignatura];
         }
+    }
+
+    async updateFromAtenea(callback: (newAsignatura: Asignatura) => void) {
+        toast.loading('Actualizando asignatura...');
+
+        const ateneaService = new AteneaService();
+        const asignaturaAtenea = await ateneaService.getAsignatura(this.asignatura.codigo);
+        if (asignaturaAtenea === null) {
+            toast.error('Error al obtener los datos de la asignatura, no se pudo cargar de Atenea');
+            return;
+        }
+
+        const uab = await dbController.getUabBy("nombre", asignaturaAtenea.uab);
+        if (uab === null) {
+            toast.error('Error al obtener los datos de la asignatura, no se pudo cargar la UAB');
+            return;
+        }
+
+        const data: Partial<Asignatura> = {
+			codigo: asignaturaAtenea.codigo,
+			nombre: asignaturaAtenea.nombre,
+			uab: uab,
+			vigente: asignaturaAtenea.vigente,
+			horasPresenciales: asignaturaAtenea.horasPresenciales,
+			horasNoPresenciales: asignaturaAtenea.horasNoPresenciales,
+			creditos: asignaturaAtenea.creditos,
+			validable: asignaturaAtenea.validable,
+			electiva: asignaturaAtenea.electiva,
+			descripcion: asignaturaAtenea.descripcion,
+			contenido: asignaturaAtenea.contenido,
+		};
+
+        this.asignatura = { ...this.asignatura, ...data };
+        callback(this.asignatura);
+        toast.success('Datos de asignatura cargados, actualice los datos necesarios');
     }
 }
