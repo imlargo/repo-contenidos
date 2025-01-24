aedbc5a7714cfb697d951212f9df2e9e9234c785
imlargo <jlargob@unal.edu.co>
2025-01-24 00:44:49 -0500

    feat: enhance asignatura loading to create from Atenea if not found in DB

--- src/routes/asignatura/+page.server.ts
+++ src/routes/asignatura/+page.server.ts
@@ -1,17 +1,28 @@
 import type { PageServerLoad } from './$types';
 import type { Asignatura } from '$src/lib/types/asignatura';
 import { dbController } from '$src/lib/db/db';
+import { AteneaService } from '$src/lib/services/atenea';
 
 export const load = (async ({ url }) => {
-	const searchCodigo = url.searchParams.get('codigo');
-	if (searchCodigo === null) {
+	const codigo = url.searchParams.get('codigo');
+	if (codigo === null) {
 		throw new Error('No se ha encontrado el código de la asignatura');
 	}
 
-	const asignatura = await dbController.getAsignatura(searchCodigo);
-
+	let asignatura = await dbController.getAsignatura(codigo);
 	if (asignatura === null) {
-		throw new Error('No se ha encontrado la asignatura');
+		const ateneaService = new AteneaService();
+		const ateneaAsignatura = await ateneaService.getAsignatura(codigo);
+		if (ateneaAsignatura === null) {
+			throw new Error('No se ha encontrado la asignatura');
+		}
+
+		const createdAsignatura = await dbController.createAsignaturaFromAtenea(ateneaAsignatura);
+		if (createdAsignatura === null) {
+			throw new Error('No se ha podido crear la asignatura');
+		}
+
+		asignatura = createdAsignatura;
 	}
 
 	return {
