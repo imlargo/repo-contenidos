57eae7a6758bbb8c29ea05fd3d9a8410e1e8b64d
imlargo <jlargob@unal.edu.co>
2025-01-23 23:20:48 -0500

    feat: add toast notifications for successful and failed updates in updateAsignatura method

--- src/lib/stores/asignatura.svelte.ts
+++ src/lib/stores/asignatura.svelte.ts
@@ -2,6 +2,7 @@ import { expresionAfirmacion } from '$src/lib/utils/enums';
 import type { Asignatura } from '$src/lib/types/asignatura';
 import { boolToAfirmacion, afirmacionToBool } from '$src/lib/utils/utils';
 import { dbController } from '$db/db';
+import { toast } from "svelte-sonner";
 
 export class StoreAsignatura {
     asignatura: Asignatura;
@@ -12,6 +13,13 @@ export class StoreAsignatura {
 
     async updateAsignatura(id: number, data: Partial<Asignatura>) {
         const result = await dbController.updateAsignatura(id, data);
+
+        if (result) {
+            toast.success('Asignatura actualizada correctamente');
+        } else {
+            toast.error('Error al actualizar los datos de la asignatura');
+        }
+
         return result;
     }
 }
